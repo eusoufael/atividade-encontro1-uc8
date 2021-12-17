3990efdb3a8268a741261b0bb6905c9b946d7b3b
Rafael Duarte <rfd.duarte@gmail.com>
2021-12-16 22:41:53 -0300

    upload

---
+++ index.js
@@ -0,0 +1,28 @@
+//idade
+let idade = 18
+if (idade=>18){
+    console.log("Idade permitida")}
+else{
+    console.log("É preciso ser maior de idade para cadastrar")
+}
+
+//data do evento
+let dataatual = "15/12/2021"
+let dataevento = "20/12/2021"
+
+if (dataatual=>dataevento){
+    console.log("Evento deve ser em uma data futura")}
+else{
+    console.log("Data permitida")
+}
+
+//participantes
+let listadeparticipantes = ["Maria", "João", "Gabriel", "Isabela", "Antônio", "Eduardo", "Jéssica"]
+ if (listadeparticipantes.length>100){
+    console.log("Lista cheia. Não pode exceder 100 participantes.")}
+else{
+    console.log("Cadastro permitido")
+}
+
+//palestrantes
+let listapalestrantes = ["Professor Eugenio", "Professor Marcio"];
